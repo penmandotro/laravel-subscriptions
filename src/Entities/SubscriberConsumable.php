3f234b1fd8218f84f30f1e7d7d1a727b1d0ee5ea
penmandotro <94607136+penmandotro@users.noreply.github.com>
2021-12-22 17:12:25 +0200

    Update SubscriberConsumable.php

--- src/Entities/SubscriberConsumable.php
+++ src/Entities/SubscriberConsumable.php
@@ -1,18 +1,18 @@
 <?php
 
-namespace BestDigital\LaravelSubscriptions\Entities;
+namespace PenMan\LaravelSubscriptions\Entities;
 
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use BestDigital\LaravelSubscriptions\Contracts\PlanContract;
-use BestDigital\LaravelSubscriptions\Contracts\PlanIntervalContract;
-use BestDigital\LaravelSubscriptions\Contracts\SubscriptionContact;
-use BestDigital\LaravelSubscriptions\PlanFeature;
-use BestDigital\LaravelSubscriptions\Contracts\SubscriptionConsumableContact;
-use BestDigital\LaravelSubscriptions\Exceptions\SubscriptionErrorException;
-use BestDigital\LaravelSubscriptions\SubscriberConsumable as MasterSubscriberConsumable;
+use PenMan\LaravelSubscriptions\Contracts\PlanContract;
+use PenMan\LaravelSubscriptions\Contracts\PlanIntervalContract;
+use PenMan\LaravelSubscriptions\Contracts\SubscriptionContact;
+use PenMan\LaravelSubscriptions\PlanFeature;
+use PenMan\LaravelSubscriptions\Contracts\SubscriptionConsumableContact;
+use PenMan\LaravelSubscriptions\Exceptions\SubscriptionErrorException;
+use PenMan\LaravelSubscriptions\SubscriberConsumable as MasterSubscriberConsumable;
 
 class SubscriberConsumable extends MasterSubscriberConsumable implements SubscriptionConsumableContact
 {
